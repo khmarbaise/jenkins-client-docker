b94d5c9d16647fb84e5b6355b5ffff7104c6b625
Karl Heinz Marbaise <khmarbaise@apache.org>
2016-01-02 23:52:47 +0100

    Changed.

--- start-docker.sh
+++ start-docker.sh
@@ -1,2 +1,7 @@
-docker run -v "$(pwd)/jobs":/var/jenkins_home/jobs -d -p 8080:8080 -p 50000:50000 jenkins-with-plugins
-#docker run -d -p 8080:8080 -p 50000:50000 jenkins-with-plugins
+docker run --name jenkins-for-testing \
+  -v "$(pwd)/jobs":/var/jenkins_home/jobs \
+  -d \
+  -p 8080:8080 \
+  -p 50000:50000 \
+  jenkins-with-plugins
+
